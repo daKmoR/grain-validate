12dac7d448ed883217d77ac47fae3103968f897e
Thomas Allmer <d4kmor@gmail.com>
2017-12-10 16:16:27 +0100

    feat: support for ok messages

--- GrainValidateMixin.js
+++ GrainValidateMixin.js
@@ -24,6 +24,16 @@ const GrainValidateMixin = superclass => class extends GrainTranslateMixin(super
       warning: {
         type: 'Object'
       },
+      okValidators: {
+        type: 'Array'
+      },
+      okList: {
+        type: 'Array',
+        value: []
+      },
+      ok: {
+        type: 'Object'
+      },
       autoValidate: {
         type: 'Boolean',
         value: false
@@ -48,18 +58,41 @@ const GrainValidateMixin = superclass => class extends GrainTranslateMixin(super
    * Transition from Error, Warning, Info to "nothing" results in Ok
    */
   validate() {
+    this.hadValidationMessage = !!this.validationMessage;
+
     this.validateType('error');
     if (!this.error) {
       this.validateType('warning');
     }
 
-    if (this.error || this.warning) {
+    this.validationMessageList = [
+      ...this.errorList,
+      ...this.warningList,
+    ];
+    this.validationMessage = this.validationMessageList.length > 0 ? this.validationMessageList[0] : false;
+
+    if (this.hadValidationMessage && !this.validationMessage) {
+      this.validateType('ok');
+      if (this.ok) {
+        this.validationMessageList = this.okList;
+        this.validationMessage = this.ok;
+      }
+    }
+    if (this.validationMessage) {
       this.autoValidate = true;
     }
   }
 
   getErrorTranslationsKeys(data) {
-    return [`errors.${data.validator}`, `global-errors:errors.${data.validator}`]
+    return [`errors.${data.validator}`, `grain-validate:errors.${data.validator}`]
+  }
+
+  getWarningTranslationsKeys(data) {
+    return [`warnings.${data.validator}`, `grain-validate:warnings.${data.validator}`]
+  }
+
+  getOkTranslationsKeys(data) {
+    return [`oks.${data.validator}`, `grain-validate:oks.${data.validator}`]
   }
 
   /**
@@ -77,13 +110,15 @@ const GrainValidateMixin = superclass => class extends GrainTranslateMixin(super
     let validatorsProperty = `${type}Validators`;
     let listProperty = `${type}List`;
     let resultProperty = `${type}`;
+    const resultPropertyFirstLetterUppercase = resultProperty.charAt(0).toUpperCase() + resultProperty.slice(1);
+    const translationKeysGetFunctionName = `get${resultPropertyFirstLetterUppercase}TranslationsKeys`;
 
     let validators = this[validatorsProperty];
     if (!validators) {
       return;
     }
     let resultList = [];
-    let value = this.rawValue;
+    let value = this.jsValue;
     let optional = false;
     let required = function(value) {
       return (typeof value === 'string' && value !== '') || (typeof value !== 'string' && typeof value !== 'undefined');
@@ -99,24 +134,29 @@ const GrainValidateMixin = superclass => class extends GrainTranslateMixin(super
       let validatorArray = Array.isArray(validators[i]) ? validators[i] : [validators[i]];
       let validatorFunction = validatorArray[0];
       let validatorParams = validatorArray[1];
-
       if (typeof validatorFunction === 'function') {
         if (!validatorFunction(value, validatorParams) && !(optional === true && typeof value === 'string' && value === '')) {
-          let data = Object.assign({}, validatorParams, {
+          let data = {
             validator: validatorFunction.name,
+            validatorParams,
+            validatorType: type,
             fieldName: this.getFieldName(),
             name: this.$$slot.input.name,
             value
-          });
+          };
           resultList.push({
             data,
-            translationKeys: this.getErrorTranslationsKeys(data)
+            translationKeys: this[translationKeysGetFunctionName](data)
           });
         }
+      } else {
+        console.warn('That does not look like a validator function', validatorFunction);
+        console.warn('You should provide options like so errorValidators=${[[functionName, {min: 5, max: 10}]]}');
       }
     }
     this[listProperty] = resultList;
     this[resultProperty] = resultList.length > 0 ? resultList[0] : false;
+    return this[resultProperty];
   }
 
 }
